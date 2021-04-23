878f4c9e8890348afc3e8e9a8495c83ef999ab9d
Chantz-J <johnson.chantz@gmail.com>
2021-04-23 17:21:24 -0400

    Basic requires and imports to get server running locally

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+//ROUTES 
+
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.json(`Welcome to the TT-33 Anywhere Fitness server! 🚴‍♀️`)
+})
+server.get('/api', (req, res) => {
+    res.json(`The TT-33 Anywhere Fitness api! 🚴‍♀🏃‍♂️`)
+})
+
+module.exports = server
